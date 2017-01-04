5a092bf3f2ed3f3e50b8617945c0b65a364a9e0a
cjfire <cjfire@sina.cn>
2017-01-04 11:53:46 +0800

    完成

--- Download/Download/ViewController.swift
+++ Download/Download/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressView: UIProgressView!
+    private lazy var session: URLSession = {
+        
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: self, delegateQueue: nil)
+        return session
+    }()
+    
+    private var downloadTask: URLSessionDownloadTask!
+    private var cancelData: Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        progressView.progress = 0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func startBtnDidClicked(_ sender: UIButton) {
+        
+        if let cancelData = cancelData {
+            downloadTask = session.downloadTask(withResumeData: cancelData)
+        } else {
+            let url = URL(string: "http://192.168.2.197/Unity_Games_by_Tutorials_v0.1.rar")
+            downloadTask = session.downloadTask(with: url!)
+        }
+        
+        downloadTask.resume()
     }
-
-
+    
+    @IBAction func stopBtnDidClicked(_ sender: UIButton) {
+        downloadTask.cancel { (cancelData) in
+            self.cancelData = cancelData
+        }
+    }
 }
 
+extension ViewController: URLSessionDownloadDelegate {
+    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        
+        print(location)
+    }
+    
+    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+        DispatchQueue.main.async {
+            self.progressView.progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        }
+        
+//        print((bytesWritten + totalBytesWritten) / totalBytesExpectedToWrite * 100)
+    }
+}
